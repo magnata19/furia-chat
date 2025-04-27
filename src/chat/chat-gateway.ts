ffdaba512ad235473afa21c9ad690ff5f244394c
Davidson <dehpacifico@gmail.com>
2025-04-27 19:25:19 -0300

    chat configurado

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -1,35 +1,29 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Logger } from "@nestjs/common";
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({ cors: { origin: "*" } })
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer()
   server: Server;
 
-  private connectedClients: Map<string, Socket> = new Map();
+  private logger: Logger = new Logger("ChatGateway");
 
-  handleConnection(client: any, ...args: any[]): void {
-    console.log(`Cliente conectado: ${client.id}`)
-    this.connectedClients.set(client.id, client);
-    this.server.emit('userConnected', client.id);
-    this.sendConnectedUsers();
-  }
-
-  handleDisconnect(client: Socket): void {
-    console.log(`Cliente conectado: ${client.id}`)
-    this.connectedClients.delete(client.id);
-    this.server.emit('userConnected', client.id);
-    this.sendConnectedUsers();
-  }
-
-  sendConnectedUsers(): void {
-    const users = Array.from(this.connectedClients.keys());
-    this.server.emit('connectedUsers', users);
-  }
-
+  @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, payload: string): void {
-    console.log(`Mensagem recebida de ${client.id}: ${payload}`);
-    this.server.emit('newMessage', { sender: client.id, message: payload });
+    this.server.emit('msgToClient', payload, client.id);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log("init")
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client discconnected: ${client.id}`);
   }
 }
