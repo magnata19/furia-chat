4671c429f71b5a4716fa4238d59a59c4ee6bf62d
Davidson <dehpacifico@gmail.com>
2025-04-28 18:37:58 -0300

    adicionando authGuard globalmente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/config/PrismaService';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/JwtConstant';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/AuthGuard';
 
 @Module({
   imports: [JwtModule.register({
@@ -11,7 +13,12 @@ import { jwtConstants } from './constants/JwtConstant';
     secret: jwtConstants.secret,
     signOptions: { expiresIn: '1d' }
   })],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ],
   controllers: [AuthController]
 })
 export class AuthModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserDto } from './dto/UserDto';
 import { UserService } from './user.service';
+import { Public } from 'src/auth/constants/JwtConstant';
 
 @Controller('api/user')
 export class UserController {
   constructor(private userService: UserService) { }
 
   @Post('register')
+  @Public()
   async createUser(@Body() userDto: UserDto): Promise<UserDto> {
     return await this.userService.createUser(userDto);
   }
