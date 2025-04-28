40ced6f6b6b0478040d85d6948dd5a0a9c9ffe42
Davidson <dehpacifico@gmail.com>
2025-04-27 22:18:17 -0300

    concluindo registro de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserDto } from './dto/UserDto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) { }
+
+  @Post('api/register')
+  async createUser(@Body() userDto: UserDto): Promise<UserDto> {
+    return await this.userService.createUser(userDto);
+  }
+}
